63227320d2d0e5a7feba85d7badae4d746b0f207
QIUZHILEI <18304935041@163.com>
2024-10-11 17:25:41 +0800

    delete BlockSize
    Signed-off-by: QIUZHILEI <18304935041@163.com>

--- src/block_dev.rs
+++ src/block_dev.rs
@@ -1,14 +1,8 @@
 use crate::{Device, DeviceError};
 use core::fmt::Debug;
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-pub enum BlockSize {
-    Lb512 = 512,
-    Lb4096 = 4096,
-}
-
 pub trait BlockDevice: Device + Sync {
-    fn block_size(&self) -> BlockSize;
+    fn block_size(&self) -> usize;
     fn read_block(&mut self, lba: usize, buf: &mut [u8]) -> Result<(), DeviceError>;
     fn write_block(&self, lba: usize, data: &[u8]) -> Result<(), DeviceError>;
     fn information(&self) -> &dyn BlkDevInfo;
